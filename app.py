f8a9054ca7c3243d28a52cfcbe074a78f1871f56
Kaven arango <43826797+KavenArango@users.noreply.github.com>
2020-11-02 10:47:57 -0500

    post for addpet api should work now

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from disasterpets import create_app
-from flask_cors import CORS
 
 app = create_app()
-CORS(app)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
-
+    app.run(host="127.0.0.1", port=5000)
