96672127d1ec9f3129516740b058af2b6b7730ea
Steven Portillo <steven.portillo@snhu.edu>
2020-12-12 15:04:51 -0500

    Upload Imagie Api Request
    Now can upload images from react

--- disasterpets/Pets/routes.py
+++ disasterpets/Pets/routes.py
@@ -1,13 +1,18 @@
 from flask import Flask, Blueprint
-from disasterpets.Pets.resources import AddPetAPI
+from disasterpets.Pets.resources import AddPetAPI, UploadImageAPI
 
 petbp = Blueprint('petbp', __name__)
 
 addpet_view = AddPetAPI.as_view('addpet_api')
+upload_image = UploadImageAPI.as_view('uploadimage_api')
 
 petbp.add_url_rule(
     '/addpet',
     view_func= addpet_view,
     methods=['POST']
 )
-        
+petbp.add_url_rule(
+    '/uploadimage',
+    view_func= upload_image,
+    methods=['POST']
+)
