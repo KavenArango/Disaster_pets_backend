144d337edcdfb7c46583109601c80e794ac78560
Steven Portillo <steven.portillo@snhu.edu>
2020-12-12 19:41:12 -0500

    Get Image route set up

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from disasterpets import create_app
 from flask_cors import CORS
+from flask import url_for, redirect, send_file
+import os
 
 app = create_app()
 CORS(app)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_STATIC = os.path.join(APP_ROOT, 'static')
+
+@app.route('/<filename>',methods=['GET'])
+def index(filename):
+    imagePath = APP_STATIC + '\\images\\' + filename
+    return send_file(imagePath, mimetype='image/jpg')
+
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
