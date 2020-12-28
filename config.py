ae22996f2f2ddb0fc9039df1af3c2244ff20a244
Kaven arango <kavenarango12@hotmail.com>
2020-12-28 17:50:22 -0500

    uh

--- config.py
+++ config.py
@@ -1,27 +1,30 @@
 from os import environ, path
 from flask_jwt_extended import JWTManager
-
+from azure.storage.blob import BlobServiceClient
 
 
 class Config:
     # General Config
-    SECRET_KEY = 'for_the_pets'
-    FLASK_APP = environ.get('FLASK_APP')
-    FLASK_ENV = environ.get('FLASK_ENV')
+    SECRET_KEY = "for_the_pets"
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
     PROPAGATE_EXCEPTIONS = True
     BCRYPT_LOG_ROUNDS = 13
     DEBUG = True
 
-    #JWT
-    JWT_SECRET_KEY = 'for_the_pets_jwt'
+    # JWT
+    JWT_SECRET_KEY = "for_the_pets_jwt"
 
-    #JWT_BLACKLIST_ENABLED = True
-    #JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
-
+    # JWT_BLACKLIST_ENABLED = True
+    # JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
 
     # Database
-    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://pets_root:1234shan!@" \
-                                "petreunification.database.windows.net:1433/disasterpetsdb?" \
-                                "DRIVER={ODBC Driver 17 for SQL Server}"
+    SQLALCHEMY_DATABASE_URI = (
+        "mssql+pyodbc://pets_root:1234shan!@"
+        "petreunification.database.windows.net:1433/disasterpetsdb?"
+        "DRIVER={ODBC Driver 17 for SQL Server}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # UPLOAD_FOLDER =
+    # ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'raw'}
