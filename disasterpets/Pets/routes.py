3a4727e1de948dae59f8d0daaabbf16b8a20d7b5
KavenArango <kavenarango@hotmail.com>
2021-02-07 13:49:11 -0500

    working on pet editing api call

--- disasterpets/Pets/routes.py
+++ disasterpets/Pets/routes.py
@@ -1,11 +1,13 @@
 from flask import Flask, Blueprint
-from disasterpets.Pets.resources import AddPetAPI, UploadImageAPI, PetDetailAPI
+from disasterpets.Pets.resources import AddPetAPI, UploadImageAPI, PetDetailAPI, ManagePetAPI
 
 petbp = Blueprint('petbp', __name__)
 
 addpet_view = AddPetAPI.as_view('addpet_api')
 upload_image = UploadImageAPI.as_view('uploadimage_api')
 pet_detail = PetDetailAPI.as_view('petdetail_api')
+managepet_view = ManagePetAPI.as_view('managepet_api')
+
 
 petbp.add_url_rule(
     '/addpet',
@@ -22,4 +24,12 @@ petbp.add_url_rule(
     '/petdetails',
     view_func= pet_detail,
     methods = ['GET']
+)
+
+
+
+petbp.add_url_rule(
+    '/managepets',
+    view_func= managepet_view,
+    methods=['POST', 'GET']
 )
