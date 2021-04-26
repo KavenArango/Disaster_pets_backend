cfea7f6a9cd3f7a42e5ec5db654d04cb615c300b
KavenArango <kavenarango@hotmail.com>
2021-04-25 20:12:52 -0400

    added route for matching

--- disasterpets/Matching/routes.py
+++ disasterpets/Matching/routes.py
@@ -1,12 +1,22 @@
 from flask import Flask, Blueprint
-from disasterpets.Matching.resources import PetMatchAPI
+from disasterpets.Matching.resources import PetMatchAPI, ManageMatchAPI
 
 matchingbp = Blueprint('matchingbp', __name__)
 
 matching_view = PetMatchAPI.as_view('petmatch_api')
+managepetmatch_view = ManageMatchAPI.as_view('managepetmatch_api')
+
+
 
 matchingbp.add_url_rule(
     '/petmatching',
     view_func= matching_view,
     methods=['GET']
+)
+
+
+matchingbp.add_url_rule(
+    '/managepetmatch',
+    view_func= managepetmatch_view,
+    methods=['post']
 )
