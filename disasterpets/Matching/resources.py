98ba74f7c56899e69f904fc1c60e83008829a2cb
KavenArango <kavenarango@hotmail.com>
2021-04-25 20:10:10 -0400

    add match for pet api

--- disasterpets/Matching/resources.py
+++ disasterpets/Matching/resources.py
@@ -4,6 +4,9 @@ from flask import Flask, Blueprint, jsonify, request, make_response, current_app
 from disasterpets.Pets.models import Pets, PetsJoin
 from disasterpets import bcrypt, db, jwt
 from flask_restful import Resource
+from disasterpets.Matching.models import PotentialMatchJoin
+from disasterpets.Matching.schema import PotentialMatchJoinSchema
+from disasterpets.Pets.resourceFunctions import collectOnePet
 
 from disasterpets.Pets.schema import (
     PetsSchema,
@@ -29,6 +32,18 @@ from disasterpets.Pets.models import (
 from disasterpets.Pictures.models import PetImageJoin
 from disasterpets.Pictures.schema import PetsImageJoinSchema
 
+
+
+
+def addMatch(requestedData):
+    newEntery = PotentialMatchJoin(
+        petid = requestedData['id'],
+        potentialid = requestedData['potentialid'],
+    )
+    if (requestedData['match'] == 'False'):
+        newEntery['match'] = requestedData['match']
+    db.session.add(newEntery)
+    db.session.commit()
 
 
 class PetMatchAPI(Resource):
@@ -78,6 +93,83 @@ class PetMatchAPI(Resource):
                 'message': 'something went wrong try again'
             }
             return make_response(jsonify(responseObject)), 404
+
+
+
+
+
+
+
+
+class ManageMatchAPI(Resource):
+    def post(self):  # asking from db to client
+        try:
+            requestedData = request.get_json()
+            responseObject = {}
+            
+            if 'match' in requestedData: # adding role
+                
+                addMatch(requestedData, True)
+                responseObject = {
+                    'status': 'success',
+                    'message': 'New Match Has been Added'
+                }
+                
+            else: # giving back one the roles
+                
+                responseObject = {
+                    'status': 'success',
+                    'Breed': collectAllMatchForOnePet(requestedData),
+                    'message': 'Breed Has Been Returned'
+                }
+                
+            return make_response(jsonify(responseObject)), 200
+        except Exception as e:
+            print(e)
+            responseObject = {
+                'status': 'failed',
+                'message': 'something went wrong try again'
+            }
+            return make_response(jsonify(responseObject)), 404
+
+
+
+
+
+
+
+def collectAllMatchForOnePet(requestedData):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def collectAllFeaturesForOnePetbecausetheotheronedontwork(requestedData):
